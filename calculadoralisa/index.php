387cbd5d75f9a5f1a64b357c75cf86f86feac4fb
leonardopaas <32268784+leonardopaas@users.noreply.github.com>
2019-08-28 21:02:04 -0300

    Add files via upload

---
+++ calculadoralisa/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="Semantic/semantic.css">
+	<link rel="stylesheet" href="Semantic/semantic.min.css" />
+	<link rel="stylesheet " type="text/css" href="css.css">
+	<link rel="stylesheet " type="text/css" href="css.css">
+	<script type="text/javascript" src="Semantic/semantic.min.js"></script>
+	<title></title>
+</head>
+<body>
+	<section class="margem">
+		<form action="funcao.php" method="post">
+			<div class="ui input">
+	 			 <input type="text" name="ip">
+			</div>
+			<div class="ui input">
+	 			 <input type="number" min="25" max="32" name="mascara" >
+			</div>
+			<button type="submit" class="ui button">Enviar</button>
+			
+		</form>
+	</section>
+</body>
+</html>
